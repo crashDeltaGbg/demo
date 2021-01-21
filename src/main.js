7436c90a5e585a2243bc7e234d4b1dc37772bfe1
Rickard Yxelflod <69391238+crashDeltaGbg@users.noreply.github.com>
2021-01-21 14:07:06 +0100

    Lite ändringar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+/* import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css' */
+
+import './main.scss'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
